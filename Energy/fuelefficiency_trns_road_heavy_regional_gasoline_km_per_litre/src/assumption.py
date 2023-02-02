b5b4202a45f4dee0cc40255229a500a436dd988d
milocortes <hermilocg@hotmail.com>
2023-02-01 18:13:53 -0600

    add ff road_heavy_regional_gasoline_km_per_litre

---
+++ Energy/fuelefficiency_trns_road_heavy_regional_gasoline_km_per_litre/src/assumption.py
@@ -0,0 +1,5 @@
+## Unit : LGE/100km 
+
+historical_assumption = { 'fuelefficiency_trns_road_heavy_regional_gasoline_km_per_litre' : 36.37}
+
+projected_assumption = { 'fuelefficiency_trns_road_heavy_regional_gasoline_km_per_litre' : 30.64}
