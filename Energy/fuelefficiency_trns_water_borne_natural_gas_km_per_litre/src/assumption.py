5f8b4e5b1b440246cb63a4ebd1415355fb99112d
milocortes <hermilocg@hotmail.com>
2023-02-03 16:31:45 -0600

    add water borne diesel, natural gas

---
+++ Energy/fuelefficiency_trns_water_borne_natural_gas_km_per_litre/src/assumption.py
@@ -0,0 +1,5 @@
+## Unit : kg/ 100 km 
+
+historical_assumption = { 'fuelefficiency_trns_water_borne_natural_gas_km_per_litre' : 0.00001165} # Fuel consumption Ro-ro vessel propelled by methanol 0.5189 (MJ fuel/t km).
+
+projected_assumption = { 'fuelefficiency_trns_water_borne_natural_gas_km_per_litre' : 0.00001165} # Fuel consumption Ro-ro vessel propelled by methanol 0.5189 (MJ fuel/t km).
