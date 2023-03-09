082aa16eaab0e8c54cdb6bd9ebb2f158fc158529
milocortes <hermilocg@hotmail.com>
2023-03-09 13:28:10 -0600

    update frac_inen_energy

--- Energy/frac_inen_energy_electronics_natural_gas/src/build_individual_variable.py
+++ Energy/frac_inen_energy_electronics_natural_gas/src/build_individual_variable.py
@@ -12,7 +12,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 sources_path = os.path.abspath(os.path.join(dir_path,"..","raw_data" ))
 
 ## Load historical data
-historical_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data.csv")
+historical_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data.csv.bz2")
 historical_data = pd.read_csv(historical_path)
 
 ## Save historical data
@@ -25,7 +25,7 @@ save_path_historical_file = os.path.join(save_path, "historical", f"{sisepuede_v
 df_sisepuede_var_name.to_csv(save_path_historical_file, index = False)
 
 ## Load projected data
-projected_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data_projected.csv")
+projected_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data_projected.csv.bz2")
 projected_data = pd.read_csv(projected_path)
 
 ## Save historical data
--- Energy/frac_inen_energy_glass_coke/src/sectors_assumptions.py
+++ Energy/frac_inen_energy_glass_coke/src/sectors_assumptions.py
@@ -20,15 +20,16 @@ fuels_correspondence = {
         'coke' : (0.10, 'Coal and coal products (PJ)'),  
         'diesel' : (0.15, 'Oil and oil products (PJ)'),  
         'electricity' : (1.0, 'Electricity (PJ)'),
-        'gas_furnace' : (1.0, 'Heat (PJ)'), 
-        'gas_petroleum_liquid' : None, 
+        'furnace_gas' : (1.0, 'Heat (PJ)'), ## gas_furnace ----> furnace_gas
+        'hydrocarbon_gas_liquids' : None, ## gas_petroleum_liquid ----> hydrocarbon_gas_liquids
         'gasoline' : (0.15, 'Oil and oil products (PJ)'),  
         'hydrogen' : None,  
         'kerosene' : None,  
         'natural_gas' : (1.0, 'Gas (PJ)'), 
         'oil' : (0.7, 'Oil and oil products (PJ)'), 
         'solar' : (1.0, 'Other sources (PJ)'), 
-        'solid_biomass': (1.0, 'Combustible renewables and waste (PJ)')
+        #'solid_biomass': (1.0, 'Combustible renewables and waste (PJ)') ## Ya no aparece en la nueva base de datos. Cambiamos por 'Biofuels and waste (PJ)'
+        'solid_biomass': (1.0, 'Biofuels and waste (PJ)')
         }
 
 industries_sisepuede = ['recycled_rubber_and_leather', 'recycled_textiles', 'electronics', 'plastic', 'chemicals', 'cement',
@@ -66,6 +67,7 @@ industries_iea = [
 'Non-specified manufacturing',
 'Transport equipment [ISIC 29-30]']
 
+"""
 industries_correspondence = {
                 'electronics' : (1.0, 'Machinery [ISIC 25-28]'), 
                 'plastic' : (1.0, 'Rubber and plastic [ISIC 22]'), 
@@ -81,6 +83,22 @@ industries_correspondence = {
                 'paper' : (1.0, 'Paper pulp and printing [ISIC 17-18]'),
                 'recycled_wood' : (1.0, 'Wood and wood products [ISIC 16]') 
                 }
+"""
+industries_correspondence = {
+  'electronics' : (0.5, 'Machinery [ISIC 25-28]'), #Electronics is ISIC 26 and 27, so let's say half of 25-28
+  'plastic' : (0.5, 'Rubber and plastic [ISIC 22]'), # SEE BELOW
+  'chemicals' : (1.0, 'Chemicals and chemical products [ISIC 20-21]'), #OK
+  'cement' : (0.9, 'Non-metallic minerals [ISIC 23]'), #FIXED
+  'glass' : (0.05, 'Non-metallic minerals [ISIC 23]'), #FIXED
+  'other_product_manufacturing' : (1.0, 'Other manufacturing [ISIC 31-32]'), #OK
+  'agriculture_and_livestock' : (1.0, 'Agriculture forestry and fishing [ISIC 01-03]'), #OK
+  'textiles' : (0.5, 'Textiles and leather [ISIC 13-15]'), #SEE BELOW
+  'rubber_and_leather' : (0.5, 'Textiles and leather [ISIC 13-15]'), #+ AND 0.5  'Rubber and plastic [ISIC 22]'
+  'metals' : (1.0, 'Basic metals [ISIC 24]'), #OK
+  'lime_and_carbonite' : (0.05, 'Non-metallic minerals [ISIC 23]'), #FIXED
+  'paper' : (1.0, 'Paper pulp and printing [ISIC 17-18]'),
+  'recycled_wood' : (1.0, 'Wood and wood products [ISIC 16]') #I propose we leave this out entirely, unless we are doing all recyclables
+}
 
 industries_correspondence_recycled = {
                 'rubber_and_leather' : 'recycled_rubber_and_leather', 
