2bf5559c9d52af57cd7ecc949e3369f9ca26bf8c
milocortes <hermilocg@hotmail.com>
2023-03-17 18:46:14 -0600

    fueleff_trns_water_borne_hydrogen_km_per_litre

---
+++ Energy/fuelefficiency_trns_water_borne_hydrogen_km_per_litre/src/assumption.py
@@ -0,0 +1,5 @@
+## Unit : kg/ 100 km 
+
+historical_assumption = { 'fuelefficiency_trns_water_borne_diesel_km_per_litre' : 0.0111} # Fuel consumption Ro-ro vessel propelled by diesel fuels 0.4987 (MJ fuel/t km).
+
+projected_assumption = { 'fuelefficiency_trns_water_borne_diesel_km_per_litre' : 0.0111} # Fuel consumption Ro-ro vessel propelled by diesel fuels 0.4987 (MJ fuel/t km).
