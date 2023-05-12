2cc7f719e5e5c723e7f81398e59e735c9a4fe644
milocortes <hermilocg@hotmail.com>
2023-05-12 10:52:18 -0600

    agrega Turquía frac_inen_energy

--- Energy/frac_inen_energy_recycled_glass_electricity/src/build_individual_variable.py
+++ Energy/frac_inen_energy_recycled_glass_electricity/src/build_individual_variable.py
@@ -15,26 +15,21 @@ sources_path = os.path.abspath(os.path.join(dir_path,"..","raw_data" ))
 historical_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data.csv.bz2")
 historical_data = pd.read_csv(historical_path)
 
-## Save historical data
-df_sisepuede_var_name = historical_data.query(f"frac_inen_energy_ == '{sisepuede_var}'")[["Country", "iso_code3", "variable", "frac_inen"]]
-df_sisepuede_var_name.rename(columns = {"Country" :  "Nation", "variable" : "Year", "frac_inen" : sisepuede_var}, inplace = True)
-
-save_path = os.path.abspath(os.path.join(dir_path,"..","input_to_sisepuede" ))
-save_path_historical_file = os.path.join(save_path, "historical", f"{sisepuede_var}.csv") 
-
-df_sisepuede_var_name.to_csv(save_path_historical_file, index = False)
-
 ## Load projected data
 projected_path = os.path.join(sources_path,"frac_inen_sisepuede_industry_fuels_data_projected.csv.bz2")
 projected_data = pd.read_csv(projected_path)
 
 ## Save historical data
-df_sisepuede_var_name = projected_data.query(f"frac_inen_energy_ == '{sisepuede_var}'")[["Country", "iso_code3", "variable", "frac_inen"]]
-df_sisepuede_var_name.rename(columns = {"Country" :  "Nation", "variable" : "Year", "frac_inen" : sisepuede_var}, inplace = True)
+save_path_historical = os.path.abspath(os.path.join(dir_path,"..","input_to_sisepuede" ,"historical" )) 
+save_path_historical_file = os.path.join(save_path_historical, f"{sisepuede_var}.csv") 
 
+country_eeih_fuel_industries_cw_long_historical = historical_data[["iso_code3", "Nation", "Year", sisepuede_var]].query("Year<=2020")
+
+country_eeih_fuel_industries_cw_long_historical.to_csv(save_path_historical_file, index = False)
+
+## Save historical data
 save_path = os.path.abspath(os.path.join(dir_path,"..","input_to_sisepuede" ))
 save_path_projected_file = os.path.join(save_path, "projected", f"{sisepuede_var}.csv") 
 
-df_sisepuede_var_name.to_csv(save_path_projected_file, index = False)
-
-
+country_eeih_fuel_industries_cw_long_projected = projected_data[["iso_code3", "Nation", "Year", sisepuede_var]].query("Year>2020")
+country_eeih_fuel_industries_cw_long_projected.to_csv(save_path_projected_file, index = False)
