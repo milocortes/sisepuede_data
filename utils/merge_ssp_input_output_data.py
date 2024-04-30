fe3cabfcf23469862a0f787c9298a94badb617f9
Hermilo Cortes <hermilocg@hotmail.com>
2024-04-29 21:32:43 -0500

    sube merge data input output

---
+++ utils/merge_ssp_input_output_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df_input = pd.read_csv("MODEL_INPUT.csv")
+df_output = pd.read_csv("MODEL_OUTPUT.csv")
+df_complete = pd.concat([df_input, df_output[df_output.columns[3:]]])
+df_complete.to_csv("sisepuede_results_sisepuede_run_ssp.csv", index = False)
