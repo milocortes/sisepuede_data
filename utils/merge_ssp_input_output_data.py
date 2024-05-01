cf2a01b9bdb3d91c81bb4b9f2c020d6c5aeb93b5
Hermilo Cortes <hermilocg@hotmail.com>
2024-04-30 21:17:19 -0500

    FIX path merge

--- utils/merge_ssp_input_output_data.py
+++ utils/merge_ssp_input_output_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_input = pd.read_csv("MODEL_INPUT.csv")
-df_output = pd.read_csv("MODEL_OUTPUT.csv")
+df_input = pd.read_csv("/opt/SSP_RESULTS/MODEL_INPUT.csv")
+df_output = pd.read_csv("/opt/SSP_RESULTS/MODEL_OUTPUT.csv")
 df_complete = pd.concat([df_input, df_output[df_output.columns[3:]]], axis =1)
-df_complete.to_csv("sisepuede_results_sisepuede_run_ssp.csv", index = False)
+df_complete.to_csv("/opt/SSP_RESULTS/sisepuede_results_sisepuede_run_ssp.csv", index = False)
