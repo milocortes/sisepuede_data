eef1d9f958502ab571b54ad976835b8a6c55cf9f
JuanRobledo12 <jroblar98@gmail.com>
2024-10-08 14:09:46 -0600

    lvst-fix-metadata-done

--- AFOLU_data_preprocessing_scripts/initial_lvst_preprocessing.py
+++ AFOLU_data_preprocessing_scripts/initial_lvst_preprocessing.py
@@ -7,82 +7,83 @@ Author: Juan Antonio Robledo
 
 import pandas as pd
 import os
+from metadata_updater import MetadataUpdater
 
-# function to handle special cases
-def handle_special_cases(row):
-    if row['Nation'] == 'China, mainland' or row['Nation'] == 'China, Taiwan Province of' or row['Nation'] == 'Sudan (former)':
-        return row['Nation']  # Copy the Nation name to the iso_code3 column
-    elif row['Nation'] == 'Netherlands (Kingdom of the)':
-        row['Nation'] = 'Netherlands'  # Change Nation name to 'Netherlands'
-        return 'NLD'  # Set iso_code3 to 'NLD'
-    elif row['Nation'] == 'United Kingdom of Great Britain and Northern Ireland':
-        return 'GBR'  # Set iso_code3 to 'GBR'
-    elif row['Nation'] == 'Türkiye':
-        return 'TUR'  # Set iso_code3 to 'TUR'
-    else:
-        return row['iso_code3']  # Return the existing ISO3 code if no special case
+# Set paths for raw data
+raw_data_path = '../AFOLU/pop_lvst_raw_data'
+raw_data_file_name = 'FAOSTAT_livestock_data.csv'
 
-raw_data_path = '../AFOLU/pop_lvst_data_raw'
-lvst_df = pd.read_csv(os.path.join(raw_data_path, 'FAOSTAT_data.csv'))
+# Import raw data csv
+lvst_raw_df = pd.read_csv(os.path.join(raw_data_path, raw_data_file_name))
 
 # Drop irrelevant columns
-lvst_relevant_df = lvst_df[['Area', 'Year', 'Item', 'Value']]
-lvst_relevant_df = lvst_relevant_df.rename(columns={'Area':'Nation'})
-
-# Load m49 JSON to get ISO3 country codes.
-df_json = pd.read_json(os.path.join(raw_data_path, 'm49-countries.json'))
-
-# Create a new column with the ISO3 country codes
-lvst_merged_df = lvst_relevant_df.merge(df_json, how='left', left_on='Nation', right_on='country_name_en')
-lvst_merged_df = lvst_merged_df[['ISO3', 'Nation', 'Year', 'Item', 'Value']]
-lvst_merged_df = lvst_merged_df.rename(columns={'ISO3': 'iso_code3'})
-
-# Apply the function to deal with ISO3 special cases to the rows
-lvst_merged_df['iso_code3'] = lvst_merged_df.apply(handle_special_cases, axis=1)
+lvst_df = lvst_raw_df[['Area Code (ISO3)', 'Area', 'Year', 'Item', 'Value']]
+lvst_df = lvst_df.rename(columns={'Area Code (ISO3)':'iso_code3', 'Area':'Nation'})
 
 # Create a dictionary from items_classification.csv to decide the format and which livestock items we will use to create the inptus for sisepuede
-crosswalk_df = pd.read_csv(os.path.join(raw_data_path,'items_classification.csv'))
-crosswalk_df_dict = dict(zip(crosswalk_df['Item_Fao'], crosswalk_df['File_Sisepuede']))
+cw_df = pd.read_csv(os.path.join(raw_data_path,'items_classification.csv'))
+cw_dict = dict(zip(cw_df['Item_Fao'], cw_df['File_Sisepuede']))
+
+# Filter the data to only get the livestock items in the cw file
+lvst_df = lvst_df[lvst_df.Item.isin(cw_df.Item_Fao)]
+
+# Create a new column with the sisepuede item names
+lvst_df["sisepuede_item"] = lvst_df["Item"].replace(cw_dict)
+
+# Perform a groupby in case several Items from FAO share the same sisepuede item
+# lvst_grouped_df = lvst_df.groupby(["iso_code3", "Nation","Year","sisepuede_item"])["Value"].mean().reset_index()  # The groupby is creating an error in the measurements it is not yet necessary
+lvst_pivot_df = lvst_df.pivot_table(index=['iso_code3', 'Nation', 'Year'], columns='sisepuede_item', values='Value').reset_index()  
+lvst_pivot_df["pop_lvst_initial_cattle_dairy"] = lvst_pivot_df["pop_lvst_initial_cattle_nondairy"].copy()
 
 # Generate new csv files for each selected livestock item
-for lvst_item in crosswalk_df_dict.keys():
+for sisepuede_var_name in [i for i in lvst_pivot_df.columns if "pop_lvst" in i]:
     
-    # Generate historical input data
-    historical_lvst_df = lvst_merged_df[lvst_merged_df.Item == lvst_item]
-    sisepuede_input_name = crosswalk_df_dict[lvst_item]
-
-    # Apply bfill and ffill within each country group (grouped by 'iso_code3') only if there are null values
-    if historical_lvst_df['Value'].isnull().any():
-        historical_lvst_df['Value'] = historical_lvst_df.groupby('iso_code3')['Value'].apply(lambda group: group.bfill().ffill())
-    
-    # Rename and drop columns to match SISEPUEDE format
-    historical_lvst_df = historical_lvst_df.drop(columns=['Item'])
-    historical_lvst_df = historical_lvst_df.rename(columns={'Value': sisepuede_input_name})
+    if lvst_pivot_df[sisepuede_var_name].isna().any():   
+        lvst_pivot_df[sisepuede_var_name] = lvst_pivot_df[sisepuede_var_name].fillna(0)
 
     # Save the new df in its directory.
-    dir_path = f'../AFOLU/{sisepuede_input_name}/input_to_sisepuede'
-    historical_lvst_df.to_csv(os.path.join(dir_path, f'historical/{sisepuede_input_name}.csv'), index = False)
-    
-    # Generate projected input data
-    max_year = historical_lvst_df.Year.max()
+    dir_path = f'../AFOLU/{sisepuede_var_name}/input_to_sisepuede'    
+    lvst_pivot_df[["iso_code3","Nation", "Year",sisepuede_var_name]].to_csv(os.path.join(dir_path,f"historical/{sisepuede_var_name}.csv"), index = False)
 
-    projected_lvst_df = historical_lvst_df[historical_lvst_df.Year == max_year]
+# Generate projected input data
+max_year = lvst_pivot_df.Year.max()
 
-    projected_lvst_df = projected_lvst_df.drop(columns = "Year")
+projected_lvst_df = lvst_pivot_df[lvst_pivot_df.Year == max_year]
 
-    projected_years_df = pd.DataFrame({"Year" : range(max_year+1, 2051)})
-    projected_lvst_df = projected_lvst_df.merge(projected_years_df, how = "cross") 
+projected_lvst_df = projected_lvst_df.drop(columns = "Year")
 
-    projected_lvst_df.to_csv(os.path.join(dir_path, f"projected/{sisepuede_input_name}.csv"), index = False)
+projected_years_df = pd.DataFrame({"Year" : range(max_year+1, 2051)})
+projected_lvst_df = projected_lvst_df.merge(projected_years_df, how = "cross") 
 
-    # Generate file for cattle non-dairy
-    if lvst_item == 'Cattle':
-        """
-        Cattle,pop_lvst_initial_cattle_nondairy,I assume 50% of cattle in FAO is nondairy
 
-        This just copies the cattle dairy to non-dairy. Perhaps we need to change this to something else
-        """
-        historical_lvst_df.to_csv(os.path.join(dir_path, f'historical/pop_lvst_initial_cattle_nondairy.csv'), index = False)
-        projected_lvst_df.to_csv(os.path.join(dir_path, f"projected/pop_lvst_initial_cattle_nondairy.csv"), index = False)
+for sise_var in [i for i in lvst_pivot_df.columns if "pop_lvst" in i]:
+    dir_path = f'../AFOLU/{sise_var}/input_to_sisepuede'
+    projected_lvst_df[["iso_code3","Nation", "Year",sise_var]].to_csv(os.path.join(dir_path,f"projected/{sise_var}.csv"), index = False)
+
+# Update Metadata
+variable_info = {
+    'name': '',
+    'subsector': 'Agriculture',
+    'longname': 'Initial Livestock',
+    'units': 'number of animals'
+}
+
+resource_info = {
+    'url': 'https://www.fao.org/faostat/en/#data/QCL',
+    'descrip': 'FAOSTAT Crops and livestock products'
+}
+
+additional_info = {
+    'assumptions': 'The projected values are equal to the last historical value.'
+}
+
+for sise_var in [i for i in lvst_pivot_df.columns if "pop_lvst" in i]:
+
+    dir_path = f'../AFOLU/{sise_var}/docs'
+    # update the YAML file for each crop item
+    variable_info['name'] = sise_var
+    updater = MetadataUpdater(os.path.join(dir_path,'metadata.yml'))
+    updater.update_yml(variable_info, resource_info, additional_info)
+
 
 print('Initial livestock data has been updated!')
