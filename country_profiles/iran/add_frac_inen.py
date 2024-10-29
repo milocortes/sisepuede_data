b83f22f039943f41afed586cf6c7a5b3c22a4c84
Hermilo Cortes <hermilocg@hotmail.com>
2024-10-29 11:10:45 -0600

    actualiza frac inen iran

--- country_profiles/iran/add_frac_inen.py
+++ country_profiles/iran/add_frac_inen.py
@@ -1,8 +1,45 @@
 import pandas as pd 
+import numpy as np
+
 
 df = pd.read_csv("real_data.csv")
 frac_inen = pd.read_csv("frac_inen_updated.csv")
+frac_inen = frac_inen.iloc[:36]
 
-df[frac_inen.columns] = frac_inen
+turquia = pd.read_csv("real_data_turkey.csv")
 
-df.to_csv("frac_inen_updated.csv", index = False)
+# Agregamos datos de Sonya al real data
+
+frac_inen = frac_inen.replace(np.nan, 0.0)
+
+#df[[i for i in frac_inen.columns if "frac_inen" in i]] = turquia[[i for i in frac_inen.columns if "frac_inen" in i]]
+
+df[[i for i in frac_inen.columns if "frac_inen" in i]] = frac_inen[[i for i in frac_inen.columns if "frac_inen" in i]]
+
+# Asignaremos el grupo de variables frac_inen_energy_agriculture_and_livestock, frac_inen_energy_cement, frac_inen_energy_glass
+# de turquia a los datos de iran
+
+turquia_imputa = ["frac_inen_energy_agriculture_and_livestock", "frac_inen_energy_cement", "frac_inen_energy_glass"]
+
+for ti in turquia_imputa:
+    ti_todo = [i for i in turquia.columns if ti in i]
+    df[ti_todo] = turquia[ti_todo]
+
+
+map_reciclado = {"frac_inen_energy_recycled_glass" : "frac_inen_energy_glass",
+"frac_inen_energy_recycled_metals" : "frac_inen_energy_metals",
+"frac_inen_energy_recycled_paper" : "frac_inen_energy_paper", 
+"frac_inen_energy_recycled_plastic" : "frac_inen_energy_plastic",  
+"frac_inen_energy_recycled_rubber_and_leather" : "frac_inen_energy_rubber_and_leather", 
+"frac_inen_energy_recycled_textiles" : "frac_inen_energy_textiles", 
+"frac_inen_energy_recycled_wood" : "frac_inen_energy_wood"}
+
+for k,v in map_reciclado.items():
+    vars_normales = [i for i in df.columns if v in i]
+    vars_reciclado = [i for i in df.columns if k in i]
+
+    df[vars_reciclado] = df[vars_normales]
+
+
+df.to_csv("real_data.csv", index = False)
+
--- country_profiles/iran/fix_gdp_pop.py
+++ country_profiles/iran/fix_gdp_pop.py
@@ -3,6 +3,7 @@ import os
 from typing import List 
 import matplotlib.pyplot as plt 
 import numpy as np 
+from scipy.interpolate import UnivariateSpline
 
 
 def build_path(PATH : List[str]) -> str:
@@ -20,21 +21,42 @@ gdp = pd.read_excel(GDP_POP_PATH, skiprows=2).rename(columns={"Unnamed: 0" : "fi
 
 gdp_long = gdp.set_index("field").T.loc[2015:]
 
+### Actualizamos el GDP para que a partir de 2020 crezca a una tasa del 3% 
+
+gdp_actualizado = [df["gdp_mmm_usd"].iloc[5]]
+tasa_crecimiento = 0.03
+
+for i in range(30):
+    gdp_actualizado.append(
+        gdp_actualizado[-1]*(1+tasa_crecimiento)
+    )
+
+gdp_actualizado = list(df["gdp_mmm_usd"].iloc[:5]) + gdp_actualizado
+
+## Suaviza serie
+
+spl = UnivariateSpline(range(36), gdp_actualizado)(range(36))
+
 ## Checa gdp
-plt.plot(gdp_long["Nominal GDP (billion US$)"].to_numpy(), label = "iran")
-plt.plot(df["gdp_mmm_usd"].to_numpy(), label = "ssp")
+plt.plot(gdp_actualizado, label = "GDP actualizado")
+plt.plot(df["gdp_mmm_usd"].to_numpy(), label = "GDP ssp")
+plt.plot(spl, label = "GDP suavizado")
 plt.legend()
 plt.show()
 
 ## Agrega gdp
-#df["gdp_mmm_usd"] = gdp_long["Nominal GDP (billion US$)"].to_numpy()
+df["gdp_mmm_usd"] = spl
 
 ## Checa población
 iran_pop_total = gdp_long["Population (million)"].to_numpy()*1_000
+iran_pop_total_empty = np.copy(iran_pop_total)
+iran_pop_total_empty[[6]] = 88213811.5
+iran_pop_suavizado = UnivariateSpline(range(36), iran_pop_total_empty)(range(36))
 ssp_iran_pop_total = df[["population_gnrl_rural", "population_gnrl_urban"]].sum(axis = 1).to_numpy()
 
 plt.plot(ssp_iran_pop_total, label = "ssp")
 plt.plot(iran_pop_total, label = "iran")
+plt.plot(iran_pop_total_empty, label = "iran suavizado")
 plt.legend()
 plt.show()
 
